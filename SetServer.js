93caf951a1cacd4638b1ad0eb00e66cd16976a47
Ivan Kichigin <ivan.kichigin977@gamil.com>
2024-02-06 20:22:29 +0800

    Added local tests wih axios

--- SetServer.js
+++ SetServer.js
@@ -2,18 +2,21 @@ const express = require("express");
 const server = express();
 server.use(express.json());
 
+const axios = require('axios');
+
+
 const tokens = require("jsonwebtoken");
 
 let users = [];
 
 server.post("/user/register", async (params, answer) => {
   try {
-    const {name, pass} = req.params;
+    const {name, pass} = params.body;
     if(!name || !pass)
       return answer.status(400).json({status:"error", message:"Введите данные пользователя!"});
 
     if (users.find(user => user.name === name))
-      return res.status(400).json({message:'Пользователь с таким именем уже существует'});
+    return res.status(400).json({message:"Пользователь с таким именем уже существует"});
 
     const token = tokens.sign({name}, "somekey");
     const user = { name: name , token: token, pts: 0};
@@ -27,4 +30,24 @@ server.post("/user/register", async (params, answer) => {
     });
   }
 
+});
+
+server.get("/", async (params, answer) => {
+  answer.send("<h1>Главная страница</h1>");
+});
+
+server.listen(5000, () => console.log("Запущен на порте 5000"));
+
+
+const userData = {
+  name: 'username',
+  pass: 'password123'
+};
+
+axios.post('http://localhost:5000/user/register', userData)
+.then(response => {
+  console.log(response.data); // Выводим ответ от сервера
+})
+.catch(error => {
+  console.error('Ошибка:', error);
 });
